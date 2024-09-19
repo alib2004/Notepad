a3900c3d0acfaddef468d3784a5ec0f13793b57a
alib2004 <aliw.b2004@gmail.com>
2024-09-19 20:26:23 +0330

    first structure

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: 
+  ["./index.html","./app.js"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'yekanbold':'iranyekan-bold',
+      'yekanmed':'iranyekan-medium',
+      'yekanreg':'iranyekan-regular'
+    }
+  },
+  plugins: [],
+}
+
