10697fd9ba2ad6648eba25ffcf41b23697297b58
alib2004 <aliw.b2004@gmail.com>
2025-10-07 20:20:08 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,30 @@ const colors = [
   { name: "Pink", hex: "#FFC0CB" },
   { name: "Brown", hex: "#A52A2A" },
 ];
-const allTodo = [
-  { id: 1, todo: "ورزش", done: false },
-  { id: 2, todo: "کتاب", done: false },
-  { id: 3, todo: "کار", done: false },
-];
+
 function App() {
+  const [allTodo, setAllTodo] = useState([
+    { id: 1, todo: "ورزش", done: false, color: "#00FFFF" },
+    { id: 2, todo: "کتاب", done: false, color: "#800080" },
+    { id: 3, todo: "کار", done: false, color: "#FFFF00" },
+  ]);
   const [color, setColor] = useState("#FF0000");
+  const [inputText, setInputText] = useState("");
   const todos = () => {
-    console.log("hi");
+    if (inputText.length > 1) {
+      const newTodo = {
+        id: Math.floor(Math.random() * 500),
+        todo: inputText,
+        done: false,
+        color: color,
+      };
+      setAllTodo([...allTodo, newTodo]);
+      setInputText("");
+    }
   };
+  const delTodo = (id) => {
+    setAllTodo(allTodo.filter((todo)=> todo.id !== id))
+  }
   return (
     <>
       <div className="center">
@@ -33,10 +47,10 @@ function App() {
       <div className="inputs">
         <input
           type="text"
-          name=""
-          id=""
+          value={inputText}
           placeholder="متن خود را وارد کنید..."
-          style={{backgroundColor:color}}
+          style={{ backgroundColor: color }}
+          onChange={(e) => setInputText(e.target.value)}
         />
         <CiSquarePlus size={60} onClick={() => todos()} className="addtodo" />
       </div>
@@ -61,11 +75,33 @@ function App() {
       </div>
       <div className="grid">
         {allTodo.map((todo) => (
-          <div className="box" key={todo.id}>
+          <div
+            className="box"
+            key={todo.id}
+            style={{
+              backgroundColor: todo.color,
+              opacity: todo.done ? 0.5 : 1,
+            }}
+          >
             <span className="todoText">{todo.todo}</span>
             <div className="flexBtns">
-              <MdDoneAll size={20} className="donebtn" color="white"/>
-              <FaRegTrashCan size={20} className="trashbtn"/>
+              <MdDoneAll
+                size={20}
+                className="donebtn"
+                color="white"
+                onClick={() =>
+                  setAllTodo(
+                    allTodo.map((t) =>
+                      t.id === todo.id ? { ...t, done: !t.done } : t
+                    )
+                  )
+                }
+              />
+              <FaRegTrashCan
+                size={20}
+                className="trashbtn"
+                onClick={() => delTodo(todo.id)}
+              />
             </div>
           </div>
         ))}
